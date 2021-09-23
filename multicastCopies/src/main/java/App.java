94469bf7f7f50234437ff21d0a10805d307fa8cf
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-09-24 02:02:43 +0700

    comments fixed version

--- multicastCopies/src/main/java/App.java
+++ multicastCopies/src/main/java/App.java
@@ -1,58 +1,62 @@
 import java.io.IOException;
-import java.io.InvalidObjectException;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class App{
-    private final int multicastPort = 8001;
-    static private final UUID uuid = UUID.randomUUID();
-    private final byte ttl = 120;
-    private final long timeForWork = 200000;
-    private final int timeToReceiveMessage = 200;
-    private final long timeToBecomeUnavailable = 5000;
-    private final long timeToSendMessage = 4000;
+public class App {
+    private static final int MULTICAST_PORT = 8001;
+    private static final UUID OWN_UUID = UUID.randomUUID();
+    private static final byte TTL = 120;
+    private static final long TIME_FOR_WORK = 100000;
+    private static final int TIME_TO_RECEIVE_MESSAGE = 200;
+    private static final long TIME_TO_BECOME_UNAVAILABLE = 5000;
+    private static final long TIME_TO_SEND_MESSAGE = 4000;
+    private static final int UUID_BYTE_LENGTH = 16;
+    private final InetAddress groupAddress;
     private Long startTime;
     private int numberOfCopies = 0;
     private long timeOfLastSending = 0;
-    private HashMap<String,Long> connectionsList = new HashMap<String, Long>();
+    private HashMap<String, Long> connectionsList = new HashMap<String, Long>();
+
+
+    public App(String address) throws UnknownHostException {
+        groupAddress = InetAddress.getByName(address);
+    }
 
     public void run() {
         String decodedReceivedCode = null;
 
         try {
             startTime = System.currentTimeMillis();
-            InetAddress groupAddress = InetAddress.getByName("224.1.1.1");
-            MulticastSocket multicastSocket = new MulticastSocket(multicastPort);
+            MulticastSocket multicastSocket = new MulticastSocket(MULTICAST_PORT);
             multicastSocket.joinGroup(groupAddress);
 
-            multicastSocket.setSoTimeout(timeToReceiveMessage);
-            byte[] code = new byte[16];
+            multicastSocket.setSoTimeout(TIME_TO_RECEIVE_MESSAGE);
+            byte[] code;
 
-            while (workTime()){
-                boolean isReadyToSend = codeUUID(code);
-                if(isReadyToSend && (System.currentTimeMillis() - timeOfLastSending) > timeToSendMessage ){
-                    DatagramPacket sendingMulticastDatagram = new DatagramPacket(code, code.length, groupAddress, multicastPort);
-                    multicastSocket.send(sendingMulticastDatagram, ttl);
+            while (isTimeNotRunOutStill()) {
+                code = codeUUID(OWN_UUID);
+                if ((System.currentTimeMillis() - timeOfLastSending) > TIME_TO_SEND_MESSAGE) {
+                    DatagramPacket sendingMulticastDatagram = new DatagramPacket(code, code.length, groupAddress, MULTICAST_PORT);
+                    multicastSocket.send(sendingMulticastDatagram, TTL);
                     timeOfLastSending = System.currentTimeMillis();
                 }
-                try{
-                    DatagramPacket receivedMulticastDatagram = new DatagramPacket(new byte[16], 16);
+                try {
+                    DatagramPacket receivedMulticastDatagram = new DatagramPacket(new byte[UUID_BYTE_LENGTH], UUID_BYTE_LENGTH);
                     multicastSocket.receive(receivedMulticastDatagram);
                     byte[] receivedCode = receivedMulticastDatagram.getData();
 
                     decodedReceivedCode = decodeUUID(receivedCode);
-                    if (!isUUID(decodedReceivedCode)) {
-                        throw new InvalidObjectException("");
-                    }
-                }catch (IOException invalid){
+
+                } catch (IOException invalid) {
                 }
 
                 String uniqueUUID = null;
-                if(!(decodedReceivedCode == null)){
+                if (decodedReceivedCode != null) {
                     uniqueUUID = decodedReceivedCode;
                 }
-                connectionsCleaner(System.currentTimeMillis());
+                connectionsCleaner();
                 connectionsList.put(uniqueUUID, System.currentTimeMillis());
                 int newNumberOfCopies = connectionsList.size();
                 if (newNumberOfCopies != numberOfCopies) {
@@ -62,105 +66,44 @@ public class App{
 
             }
 
-        } catch (IOException ignored) {}
+        } catch (IOException ignored) {
+        }
 
     }
 
-
-
-    private boolean workTime(){
-        return timeForWork >= (System.currentTimeMillis()-startTime);
+    private boolean isTimeNotRunOutStill() {
+        return TIME_FOR_WORK >= (System.currentTimeMillis() - startTime);
     }
 
-    private void connectionsCleaner(Long workingTime){
-        this.connectionsList.entrySet().removeIf(next -> workingTime - next.getValue() > this.timeToBecomeUnavailable);
+    private void connectionsCleaner() {
+        this.connectionsList.entrySet().removeIf(next -> System.currentTimeMillis()
+                - next.getValue() > TIME_TO_BECOME_UNAVAILABLE);
     }
 
-    private boolean isUUID(String receivedStringUUID){
-        return (receivedStringUUID != null);
+    public String decodeUUID(byte[] bytes) {
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        long firstLong = bb.getLong();
+        long secondLong = bb.getLong();
+        UUID newUUID = new UUID(firstLong, secondLong);
+        return newUUID.toString();
     }
 
-    private void printCopiesList(){
+    public byte[] codeUUID(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[UUID_BYTE_LENGTH]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    private void printCopiesList() {
         System.out.println("Copies: ");
         int counter = 0;
         for (var entry : this.connectionsList.entrySet()) {
-            if(!entry.getKey().equals(uuid.toString())){
+            if (!entry.getKey().equals(OWN_UUID.toString())) {
                 System.out.println(entry.getKey());
                 counter++;
             }
         }
         System.out.println("All in all: " + counter + " alive copies");
-    }
-
-
-
-    private void codeSymbol(String symbol, byte[] code, byte code_idx){
-        code[code_idx/2] <<= 4;
-        byte num = Byte.parseByte(symbol,16);
-        code[code_idx/2] = (byte) (code[code_idx/2] | num);
-    }
-
-
-    public boolean codeUUID(byte[] code){
-        String str = uuid.toString();
-        byte code_idx = 0;
-        String[] sub_str = str.split("-");
-        if(sub_str.length < 4){
-            return false;
-        }
-        for(int i = 0; i < 5; i++) {
-            String[] sub_str_lvl2 = sub_str[i].split("");
-            int number;
-            if(i == 0){
-                number = 8;
-            }else if (i == 4){
-                number = 12;
-            }else{
-                number = 4;
-            }
-            for(int j = 0; j < number; j++){
-                codeSymbol(sub_str_lvl2[j], code, code_idx);
-                code_idx++;
-            }
-        }
-        return true;
-    }
-
-    public String reform(byte number){
-        if(number < 10){
-            return Integer.toString(number);
-        }else{
-            switch (number){
-                case 10: return "a";
-                case 11: return "b";
-                case 12: return "c";
-                case 13: return "d";
-                case 14: return "e";
-                case 15: return "f";
-            }
-        }
-        return null;
-    }
-
-
-    public String decodeUUID(byte[] new_code){
-        byte[] code = new_code.clone();
-        StringBuilder stringBuilder = new StringBuilder();
-        int mask = 15;
-        int code_idx = 0;
-        if(new_code.length != 16){
-            return null;
-        }
-        for(int i = 0; i < 32; i++){
-            if(i == 8 || i == 12 || i == 16 || i == 20){
-                stringBuilder.append("-");
-            }
-            byte part = (byte)((code[code_idx/2] >> 4) & mask);
-            stringBuilder.append(reform(part));
-            code[code_idx/2] <<= 4;
-            code_idx++;
-        }
-
-        return stringBuilder.toString();
     }
 }
--- multicastCopies/src/main/java/Main.java
+++ multicastCopies/src/main/java/Main.java
@@ -1,9 +1,10 @@
+import java.net.UnknownHostException;
 
 public class Main {
 
 
-    public static void main(String[] args){
-        App app = new App();
+    public static void main(String[] args) throws UnknownHostException {
+        App app = new App(args[0]);
         app.run();
     }
 
